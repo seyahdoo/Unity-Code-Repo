8d9dac108c5edb8cd7431f62ac9b908b663f90a7
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2016-11-03 09:30:50 +0300

    Added Console :smile:

---
+++ Console/Console.cs
@@ -0,0 +1,179 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+/// <summary>
+/// Must be attached to an object to be worked...
+/// </summary>
+public class Console : MonoBehaviour {
+
+    public bool IsListening = false;
+    public string cmd = "";
+
+    void Update()
+    {
+        //if get key "  listen to the keystokes, if " again ignore, if [Enter] Excecute.
+        if (Input.GetKeyDown(KeyCode.BackQuote) && !IsListening)
+        {
+            IsListening = true;
+            cmd = "";
+        }
+        else if (Input.GetKeyDown(KeyCode.BackQuote) && IsListening)
+        {
+            IsListening = false;
+            cmd = "";
+        }
+        else if (Input.GetKeyDown(KeyCode.Return) && IsListening)
+        {
+            ConsoleController.Execute(cmd);
+            IsListening = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Backspace) && IsListening)
+        {
+            //delete last character from cmd
+            cmd = cmd.Remove(cmd.Length - 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab) && IsListening)
+        {
+            //TODO
+            Debug.Log("Intellisense");
+        }
+        else if (Input.anyKeyDown && IsListening)
+        {
+            if (Input.inputString.Length > 0)
+            {
+                cmd += Input.inputString;
+            }
+        }
+        
+    }
+
+    void OnGUI()
+    {
+        if (IsListening)
+        {
+            GUI.Label(new Rect(0, 0, Screen.width, Screen.height), cmd);
+        }
+    }
+
+
+}
+
+public partial class ConsoleController
+{
+    #region Static Controller Thingies
+
+    private static ConsoleController _instance;
+
+    public static ConsoleController instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new ConsoleController();
+            }
+
+            return _instance;
+        }
+    }
+
+    public delegate void CommandHandler(string[] args);
+
+    /// <summary>
+    /// Object to hold information about each command
+    /// </summary>
+    class CommandRegistration
+    {
+        public string command { get; private set; }
+        public CommandHandler handler { get; private set; }
+        public string help { get; private set; }
+
+        public CommandRegistration(string command, CommandHandler handler, string help)
+        {
+            this.command = command;
+            this.handler = handler;
+            this.help = help;
+        }
+    }
+
+    static Dictionary<string, CommandRegistration> commands = new Dictionary<string, CommandRegistration>();
+
+    void registerCommand(string command, CommandHandler handler, string help)
+    {
+        commands.Add(command, new CommandRegistration(command, handler, help));
+    }
+
+    public void runCommandString(string commandString)
+    {
+        Debug.Log("$ " + commandString);
+
+        string[] commandSplit = parseArguments(commandString);
+        string[] args = new string[0];
+        if (commandSplit.Length < 1)
+        {
+            Debug.Log(string.Format("Unable to process command '{0}'", commandString));
+            return;
+
+        }
+        else if (commandSplit.Length >= 2)
+        {
+            int numArgs = commandSplit.Length - 1;
+            args = new string[numArgs];
+            Array.Copy(commandSplit, 1, args, 0, numArgs);
+        }
+        runCommand(commandSplit[0].ToLower(), args);
+        //commandHistory.Add(commandString);
+    }
+
+    public void runCommand(string command, string[] args)
+    {
+        CommandRegistration reg = null;
+        if (!commands.TryGetValue(command, out reg))
+        {
+            Debug.Log(string.Format("Unknown command '{0}', type 'help' for list.", command));
+        }
+        else
+        {
+            if (reg.handler == null)
+            {
+                Debug.Log(string.Format("Unable to process command '{0}', handler was null.", command));
+            }
+            else
+            {
+                reg.handler(args);
+            }
+        }
+    }
+
+    static string[] parseArguments(string commandString)
+    {
+        LinkedList<char> parmChars = new LinkedList<char>(commandString.ToCharArray());
+        bool inQuote = false;
+        var node = parmChars.First;
+        while (node != null)
+        {
+            var next = node.Next;
+            if (node.Value == '"')
+            {
+                inQuote = !inQuote;
+                parmChars.Remove(node);
+            }
+            if (!inQuote && node.Value == ' ')
+            {
+                node.Value = '\n';
+            }
+            node = next;
+        }
+        char[] parmCharsArr = new char[parmChars.Count];
+        parmChars.CopyTo(parmCharsArr, 0);
+        return (new string(parmCharsArr)).Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    public static void Execute(string cmd)
+    {
+        instance.runCommandString(cmd);
+    }
+
+    #endregion
+}
---
+++ Console/ConsoleCommands.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public partial class ConsoleController {
+
+    public ConsoleController()
+    {
+        //When adding commands, you must add a call below to registerCommand() with its name, implementation method, and help text.
+        //registerCommand("babble", babble, "Example command that demonstrates how to parse arguments. babble [word] [# of times to repeat]");
+        //registerCommand("hide", hide, "Hide the console.");
+        //registerCommand(repeatCmdName, repeatCommand, "Repeat last command.");
+        //registerCommand("reload", reload, "Reload game.");
+        //registerCommand("resetprefs", resetPrefs, "Reset & saves PlayerPrefs.");
+        registerCommand("help", help, "Print this help.");
+        registerCommand("echo", echo, "echoes arguments back as array (for testing argument parser)");
+        registerCommand("loadnext", loadNext, "Loads next level (Game manager Tester!)");
+        registerCommand("endlevel", endLevel, "Ends current level (Game manager Tester!)");
+
+    }
+
+    void help(string[] args)
+    {
+        foreach (CommandRegistration reg in commands.Values)
+        {
+            Debug.Log(string.Format("{0}: {1}", reg.command, reg.help));
+        }
+    }
+
+    void echo (string[] args)
+    {
+        Debug.Log("Echo! "+args[0]+" "+args[1]);
+    }
+
+    void loadNext (string[] args)
+    {
+        Debug.Log("Loading next level");
+        GameManager.LoadThings();
+    }
+
+    void endLevel (string[] args)
+    {
+        Debug.Log("Starting next level");
+        GameManager.EndLevel();
+    }
+
+}
