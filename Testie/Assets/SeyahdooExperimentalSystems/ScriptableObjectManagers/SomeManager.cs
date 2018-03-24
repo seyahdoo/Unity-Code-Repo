18e5cfab7c491fa06a3885534d92e2faf992a540
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2018-03-24 18:49:16 +0300

    Scriptable object managers

---
+++ Testie/Assets/SeyahdooExperimentalSystems/ScriptableObjectManagers/SomeManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Reflection;
+
+[CreateAssetMenu]
+public class SomeManager : ScriptableObject {
+
+	#region brilliant_part_pluggable_manager
+
+	public string SomeManagerSetting;
+
+	public void logit(string log){
+
+		//Some high level function
+		Debug.Log ("SomeManager: " + log);
+		Debug.Log ("SomeManager:SomeManagerSetting: " + SomeManagerSetting);
+
+	}
+
+	#endregion
+
+	#region silly_part_singleton
+	public static SomeManager Instance(){
+
+		if (_instance)
+			return _instance;
+
+		_instance = Resources.Load<SomeManager> ("Managers/" + MethodBase.GetCurrentMethod ().DeclaringType);
+		if (_instance) {
+			Debug.Log ("Returning loaded manager");
+			return _instance;
+		}
+
+		_instance = SomeManager.CreateInstance<SomeManager> ();
+		if (_instance) {
+			Debug.Log ("Returning created manager");
+			return _instance;
+		}
+		
+		return null;
+	}
+
+	static SomeManager _instance;
+
+	#endregion
+
+
+}
---
+++ Testie/Assets/SeyahdooExperimentalSystems/ScriptableObjectManagers/SomeOtherScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeOtherScript : MonoBehaviour {
+
+	public SomeManager someManager;
+
+	void Awake(){
+		InvokeRepeating ("UseManager", 0, 1);
+	}
+
+
+	void UseManager(){
+	
+		SomeManager.Instance ().logit (Time.time.ToString ());
+
+		//someManager.logit (Time.time.ToString ());
+
+	}
+
+}
