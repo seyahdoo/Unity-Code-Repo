acb0625614ef49ef61210c2b20a9299a7144fac7
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2018-10-26 21:08:12 +0300

    Added OnValidate Experiment

---
+++ Testie/Assets/Experimental/OnValidate/ValidateTester.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ValidateTester : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    //strong way to never get a null reference.
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (rb == null)
+            rb = GetComponent<Rigidbody>();
+    }
+
+#endif
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(Vector3.up * 10f);
+	}
+}
